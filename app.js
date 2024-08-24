8dc0334fb4372c6224d4f60eee172ea99bb01f0f
[NumanXdev] <9256fa@gmial.com>
2024-08-24 07:40:50 +0530

    Created updata,new,delete route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const listing = require("./models/listing");
 const path = require("path"); //ejs
+const methodOverride = require("method-override");
 
 const MONGOOSE_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -20,7 +21,9 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
   res.send("All fine");
 });
@@ -48,32 +51,55 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 });
 // Create route
-app.get("/listings/new",(req,res)=>{
+app.get("/listings/new", (req, res) => {
   // res.send("Working")
   res.render("listings/new.ejs");
 });
 
 //show route
-app.get("/listings/:id", async(req, res) => {
+app.get("/listings/:id", async (req, res) => {
   // res.send("Working")
   let { id } = req.params;
   const data = await listing.findById(id);
   // console.log(data)
-  res.render("listings/show.ejs",{data})
+  res.render("listings/show.ejs", { data });
 });
 
 //Create route
-app.post("/listings", async (req,res)=>{
-//let {title,description,price,image,country,location}=req.body
-// let listing=req.body;
-// console.log(listing)
+app.post("/listings", async (req, res) => {
+  //let {title,description,price,image,country,location}=req.body
+  // let listing=req.body;
+  // console.log(listing); ///this will return listing object
 
-const newListing= new listing(req.body);
-await newListing.save();
-redirect("/listings")
+  const newListing = new listing(req.body.listing); // this will access that and will add that to database
+
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//Update Route
+
+app.get("/listings/:id/edit", async (req, res) => {
+  // res.send("Working!")
+  let { id } = req.params;
+  const Listing = await listing.findById(id);
+  res.render("listings/edit.ejs", { Listing });
+});
+
+app.put("/listings/:id", async(req, res) => {
+  let { id } = req.params;
+  await listing.findByIdAndUpdate(id,{...req.body.listing});  //deconstruct kr k individual parameter me convert kiya
+  // const listing = req.body.listings;/
+  res.redirect(`/listings/${id}`);
+});
+
+
+//Delete route
+app.delete("/listings/:id",async(req,res)=>{
+  let {id}=req.params;
+  await listing.findByIdAndDelete(id);
+  res.redirect("/listings")
 })
-
-
 
 app.listen(8080, (req, res) => {
   console.log("Listening");
