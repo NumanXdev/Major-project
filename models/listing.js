c6a9bb64bca1dc1236649a4cef8b36f4779f59ea
Numan <9256fa@gmail.com>
2024-10-06 14:31:35 +0530

    Require review Schema and post middleware for deleting Review ObhId's

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const review = require("./review");
 //mongoose.Schema-> stored into variable Schema so that while creating schema we dont need to write as mongoose.Schema
 const Schema = mongoose.Schema;
 
@@ -11,23 +12,29 @@ const ListingSchema = new Schema({
   image: {
     type: String,
     default:
-     " https://images.pexels.com/photos/4428293/pexels-photo-4428293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://images.pexels.com/photos/4428293/pexels-photo-4428293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      
-      //image ha lekin link empty
+      " https://images.pexels.com/photos/4428293/pexels-photo-4428293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://images.pexels.com/photos/4428293/pexels-photo-4428293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+    //image ha lekin link empty
     set: (v) =>
       v === ""
-        ?  " https://images.pexels.com/photos/4428293/pexels-photo-4428293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://images.pexels.com/photos/4428293/pexels-photo-4428293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        ? " https://images.pexels.com/photos/4428293/pexels-photo-4428293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://images.pexels.com/photos/4428293/pexels-photo-4428293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         : v,
   },
   price: Number,
   location: String,
   country: String,
-  reviews:[
+  reviews: [
     {
-      type:Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Review",
-    }
-  ]
+    },
+  ],
+});
+
+ListingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await review.deleteMany({ _id: { $in: listing.reviews } });
+  }
 });
 
 const Listing = mongoose.model("Listing", ListingSchema); //listings in DB
