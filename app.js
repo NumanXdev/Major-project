377b25882663d1b369308057e53412750e45e284
Numan <9256fa@gmail.com>
2024-10-18 20:32:12 +0530

    SessionOption

--- app.js
+++ app.js
@@ -55,6 +55,11 @@ sessionOption={
   secret:"mysupersecretcode",
   resave:false,
   saveUninitialized: true,
+  cookie:{
+    expires:Date.now()+ 7 * 24 * 60 *60 *1000,
+    maxAge:  7 * 24 * 60 *60 *1000,
+    httpOnly: true,
+  }
 }
 
 app.use(session(sessionOption));
