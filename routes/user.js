d73d0519e5ccf472a65563f467d289014c4f67c0
Numan_10 <9256fa@gmail.com>
2024-10-20 14:59:31 +0530

    Add login functioality

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -19,5 +20,21 @@ router.post("/signup", async (req, res) => {
     res.redirect("/signup");
   }
 });
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome to wanderlust, You are logged in!");
+    res.redirect("/listings");
+  }
+);
 
 module.exports = router;
