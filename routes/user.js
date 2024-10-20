ff68840f3814369f67060be9246945be18c77c8e
Numan_10 <9256fa@gmail.com>
2024-10-20 14:32:24 +0530

    user Route

---
+++ routes/user.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User.js");
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+
+router.post("/signup", async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    const newUser = new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.flash("success", "Welcome to Wanderlust!");
+    res.redirect("/listings");
+  } catch (e) {
+    req.flash("error", `${e.message}!`);
+    res.redirect("/signup")
+  }
+});
+
+module.exports = router;
