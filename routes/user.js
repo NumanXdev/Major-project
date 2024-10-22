30fa41dec779273f69fad008044605e0e50ee200
Numan_10 <9256fa@gmail.com>
2024-10-22 18:21:27 +0530

    Login after Signup

--- routes/user.js
+++ routes/user.js
@@ -12,9 +12,15 @@ router.post("/signup", async (req, res) => {
     let { username, email, password } = req.body;
     const newUser = new User({ email, username });
     const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser);
-    req.flash("success", "Welcome to Wanderlust!");
-    res.redirect("/listings");
+    // console.log(registeredUser);
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      } else {
+        req.flash("success", "Logged in Successfully, Welcome to Wanderlust!");
+        res.redirect("/listings");
+      }
+    });
   } catch (e) {
     req.flash("error", `${e.message}!`);
     res.redirect("/signup");
@@ -25,7 +31,7 @@ router.get("/login", (req, res) => {
   res.render("users/login.ejs");
 });
 
-router.post(
+router.post(      
   "/login",
   passport.authenticate("local", {
     failureRedirect: "/login",
