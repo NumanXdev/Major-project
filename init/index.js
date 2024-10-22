fbd796c53bde9d9de838bf7057964fdd09d3478a
Numan_10 <9256fa@gmail.com>
2024-10-22 22:35:10 +0530

    Adding owne

--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const intdata = require("./data.js");
+const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const MONGOOSE_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -18,7 +18,12 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  await Listing.insertMany(intdata.data);   //module export data:SampleListings
+  initdata.data = initdata.data.map((obj) => ({
+    ...obj,
+    owner: "67166c05878d8e137e5ac770",
+  }));
+  await Listing.insertMany(initdata.data); //module export data:SampleListings
+
   console.log("Data initialized");
 };
 
