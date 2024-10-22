c550134a9f4210a7ca2f1520e70e7f157123c1ce
Numan_10 <9256fa@gmail.com>
2024-10-22 19:08:28 +0530

    Middleware:SaveRedirectUrl

--- Middleware.js
+++ Middleware.js
@@ -1,7 +1,17 @@
-module.exports.isLoggedIn = (req,res,next) => {
+module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    // console.log(req)
+    // RedirectUrl-> see notes
+    req.session.redirectUrl = req.originalUrl;
     req.flash("error", "You must be logged In!");
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
 };
