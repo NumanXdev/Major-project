dbac1343e3040b2501dcacfb3ad7151e4da5093a
Numan_10 <9256fa@gmail.com>
2024-10-24 12:34:14 +0530

    Authorization Middleware & Restructuring of others MW

--- Middleware.js
+++ Middleware.js
@@ -1,3 +1,8 @@
+const listing = require("./models/listing");
+const ExpressError = require("./utils/ExpressError.js");
+const { ListingSchema } = require("./schema.js");
+const { reviewSchema } = require("./schema.js");
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     // console.log(req)
@@ -14,4 +19,43 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
+};
+
+//Implementing Authorization for Listings
+module.exports.isOwner = async (req, res, next) => {
+  let { id } = req.params;
+  let Listing = await listing.findById(id);
+  //Authorization logic below
+  if (!Listing.owner.equals(res.locals.currUser._id)) {
+    req.flash("error", "You are not the owner of this listing!");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
+
+//Making function of validation using it as a middleware
+//for listing (Server side)
+module.exports.validateListing = (req, res, next) => {
+  let { error } = ListingSchema.validate(req.body);
+  if (error) {
+    // console.log(error.details);
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//for Review serverside MW
+module.exports.validateReview = (req, res, next) => {
+  // console.log(req.body);
+  let { error } = reviewSchema.validate(req.body);
+
+  if (error) {
+    console.log(error.details);
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
 };
